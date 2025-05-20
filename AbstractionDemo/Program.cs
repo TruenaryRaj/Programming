959be663f10dd692b8b756ae12269db6f0979399
TruenaryRaj <raj.truenary@gmail.com>
2025-05-20 11:30:04 +0545

    feat(abstraction): added abstract class and used it in concrete class

--- AbstractionDemo/Program.cs
+++ AbstractionDemo/Program.cs
@@ -1,2 +1,76 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+/* This program is a simple example of abstraction where Vehicle is a abstract class(base class) with abstract method
+    and Car and Bike are derived classes that implement the abstract methods.  Similarly FuelType is a property of Vehicle class (public)
+    which is accesible publicly. 
+*/
+
+
+// Abstract class Vehicle with abstract methods
+public abstract class Vehicle
+    {
+        protected string FuelType;         //declaring public property FuelType so that every derived class can access it.
+        public abstract void Start();
+        public abstract void Stop();
+        public abstract void Display();
+    }
+
+
+
+// Derived class Car implementing abstract methods
+public class Car : Vehicle
+    {
+        public override void Start()
+        {
+            Console.WriteLine("Car starting with " + FuelType);
+        }
+
+        public override void Stop()
+        {
+            Console.WriteLine("Car stopped");
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("This is a car");
+        }
+    }
+
+    // Derived class Bike implementing abstract methods
+    public class Bike : Vehicle
+    {
+        public override void Start()
+        {
+            Console.WriteLine("Bike starting with " + FuelType);
+        }
+
+        public override void Stop()
+        {
+            Console.WriteLine("Bike stopped");
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("This is a bike");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle CarObject = new Car { FuelType = "Diesel" };  //Creating object of Car class Reference of Vehicle by which only vehicle class members can be accessed
+            Vehicle BikeObject = new Bike { FuelType = "Petrol" }; // Initiaing parent property at the time of object creation.
+
+            CarObject.Display();
+            CarObject.Start();
+            CarObject.Stop();
+
+            //Line breaker for better readability
+             Console.WriteLine();
+
+            BikeObject.Display();
+            BikeObject.Start();
+            BikeObject.Stop();
+        }
+    }
