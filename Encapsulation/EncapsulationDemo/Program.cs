15fe3bb6ec52e59a01e5e4bbeaf64ef3a6f512e8
TruenaryRaj <raj.truenary@gmail.com>
2025-05-21 16:12:43 +0545

    feat(encapsulation): implement initial logic

--- Encapsulation/EncapsulationDemo/Program.cs
+++ Encapsulation/EncapsulationDemo/Program.cs
@@ -1,29 +1,28 @@
 ﻿
-/* this program is a demo of encapsulation in which there is Student class with privcate fields and public methods to access and modify the data. 
-   The class has a method to calculate the average of grades and a method to get the grades. 
-   The program creates an instance of the Student class, adds grades, calculates the average, and displays the grades.
- 
- */
+
+
+
+
 
 
 public class Student
 {
     private string name;  //creating a private field to store name
-    private List<int> grades = new List<int>(); //creating a private list to store grades
+    private List<int> grades; //creating a private list to store grades
+    private  List<string> subject;
 
-    public string Name   //public property to access the private field name
+
+   public Student(string name)
     {
-        get { return name; }
-        set {
-
-            if (string.IsNullOrEmpty(value))
-            {
-                Console.WriteLine("Name cannot be null or empty");
-            }
-            name = value;  //private name is assigned to the value received from the property
-        }
+        this.name = name;//constructor to initialize the name
+        this.grades = new List<int>();
+        this.subject = new List<string>(); //initializing the list of grades
     }
 
+    public string Name
+    {
+        get { return name; }
+    }
 
     //method to add a grade to the list
     public void AddGrade(int grade)
@@ -33,6 +32,16 @@ public class Student
             Console.WriteLine("Grade must be between 0 and 100"); 
         }
         grades.Add(grade);
+    }
+
+    //method to add a subject to the list
+    public void AddSubject(string subjectName)
+    {
+        if (string.IsNullOrEmpty(subjectName))
+        {
+            Console.WriteLine("Subject name cannot be empty");
+        }
+        subject.Add(subjectName);
     }
 
     //method to calculate the average of the grades
@@ -46,10 +55,17 @@ public class Student
         return total / grades.Count;
     }
 
+
+
     //method to get the grades 
     public IReadOnlyList<int> GetGrade()
     {
         return  grades;
+    }
+
+    public IReadOnlyList<string> GetSubject()
+    {
+        return subject;
     }
 }
 
@@ -57,16 +73,54 @@ public class Program
 {
     static void Main()
     {
-        Student StudentObject = new Student(); //creating an instance of the Student class
-        StudentObject.Name = "Ram";  // assigning the name to the student
-        StudentObject.AddGrade(85);   // assigning the grades to the student
-        StudentObject.AddGrade(90);
-        StudentObject.AddGrade(78);
-        double average = StudentObject.CalculateAverage();  //calling method to calculate average
-        Console.WriteLine($"Average grade for {StudentObject.Name} is {average}");
+        List<Student> StudentObject = new List<Student>(); //creating an instance of the Student class
 
-        IReadOnlyList<int> displayGrades = StudentObject.GetGrade();    //calling method to get the grade list
-        Console.WriteLine("Grades: " + string.Join(", ", displayGrades));
+        Console.WriteLine("Enter number of students:");
+        int numberOfStudents = Convert.ToInt32(Console.ReadLine());
+        for (int j = 0; j < numberOfStudents; j++)
+        {
 
+            Console.WriteLine($"Enter detail for student{j + 1}");
+            Console.WriteLine("Enter student name:");
+            string name = Console.ReadLine();
+            Student student = new Student(name);//adding the student name to the list
+            StudentObject.Add(student);
+
+            Console.WriteLine("Enter total number of subject:");
+            int totalSubject = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < totalSubject; i++)
+            {
+                Console.WriteLine("Enter subject name:");
+                string subjectName = Console.ReadLine();
+                StudentObject[j].AddSubject(subjectName);
+            }
+
+            var subject = StudentObject[j].GetSubject(); //calling method to get the subject list
+
+            Console.WriteLine("Enter Grades for each subject:");
+            for (int i = 0; i < totalSubject; i++)
+            {
+                Console.WriteLine($"Enter grade for {subject[i]}");
+                int grade = Convert.ToInt32(Console.ReadLine());
+                StudentObject[j].AddGrade(grade);
+            }
+
+
+
+            double average = StudentObject[j].CalculateAverage();  //calling method to calculate average
+            Console.WriteLine($"Average grade of {StudentObject[j].Name} is from {totalSubject} is {average}");
+
+            var displayGrades = StudentObject[j].GetGrade();    //calling method to get the grade list
+
+            Console.WriteLine("Each Subject with it's grade:");
+            for (int i = 0; i < totalSubject; i++)
+            {
+                Console.WriteLine($"{subject[i]}: {displayGrades[i]}");
+            }
+
+        }
+
+        Console.ReadLine();
     }
 }
