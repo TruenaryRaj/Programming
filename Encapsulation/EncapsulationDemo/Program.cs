e035861ff440a52e39a14e5d27cc593fa3123aac
TruenaryRaj <raj.truenary@gmail.com>
2025-05-23 12:58:56 +0545

    feat(encapsulation): implement initial logic

--- Encapsulation/EncapsulationDemo/Program.cs
+++ Encapsulation/EncapsulationDemo/Program.cs
@@ -3,48 +3,54 @@
 have to use the public methods of the class. 
 
 dictionary is being use to store the name of the student and list of their respective grade, one student can have multiple grades.
-Once the instance of the student class is created, we can add the student name and their grades using the AddStudent method and once 
-the grade are added, it calls the average calculation method and displays the average of all grades.
+Once the instance of the student class is created, as we add the student name by the help of AddStudent method then we call the AddGrade method to add the 
+repective level grade after the grade check and after that we call the CalculateAverage method to calculate the average of the grades.
  */
 
+using System.Xml.Linq;
 
 public class Student
 {
-    private Dictionary<string, List<int>> SubjectGrades;
-
-    public Student()
+    private Dictionary<string, List<Dictionary<string, int>>> SubjectGrades = new Dictionary<string, List<Dictionary<string, int>>>();/*
     {
-        SubjectGrades = new Dictionary<string, List<int>>();
+        {
+                string.Empty, new List<Dictionary<string, int>>()
+                {
+                    new Dictionary<string, int>() { { "C", 0 }, { "C#", 0 }, {"Java",0 } }
+                }
+            },
+    };*/
+    private void AddGrades( string name)
+    {
+        List<int> grade;
+        Console.WriteLine($"Enter the grades for C, c#, java in this format");
+        string input = Console.ReadLine();
+        grade = input.Split(',').Select(s => int.Parse(s.Trim())).ToList();
+      
+        SubjectGrades[name] = new List<Dictionary<string, int>>()
+        {
+            new Dictionary<string, int>() { { "C", grade[0] }, { "C#", grade[1] }, {"Java",grade[2] } }
+        };
     }
-
     public void AddStudent(string name)
     {
-        Console.WriteLine("Level 11 or 12:");
-        int Level = Convert.ToInt32(Console.ReadLine());
-        List<int> grade;
+        Console.WriteLine($"Welcome {name}");
         
-        if (Level == 11)
-        {
-                Console.WriteLine("Enter the subjects and grades in the format: Math,  DSA, C ");
+        AddGrades(name);
 
-                string input =Console.ReadLine();
-                grade = input.Split(',').Select(s => int.Parse(s.Trim())).ToList();
-                SubjectGrades[name] = grade;
-        }else
-        {
-                Console.WriteLine("Enter the subjects and grades in the format: Python,  Java, C# ");
-
-                string input = Console.ReadLine();
-                grade = input.Split(',').Select(s => int.Parse(s.Trim())).ToList();
-                SubjectGrades[name] = grade;
-        }
         CalculateAverage(name);
     }
  
     private void CalculateAverage(string name)
     {
-         List<int> list = SubjectGrades[name];
-         Console.WriteLine($"Average of {name} is {list.Average():f2}");
+        List<int> grades = SubjectGrades[name][0].Values.ToList();
+
+        Console.WriteLine($"Average of {name} is {grades.Average() } ");
+
+        foreach (var subject in SubjectGrades[name][0]) 
+        {
+            Console.WriteLine($"{subject.Key} ->{subject.Value}");
+        }
     }
 }
 
@@ -54,6 +60,9 @@ public class Program
     {
         Student student1 = new Student();
         student1.AddStudent("Ram");
+
+        student1.AddStudent("Shyam");
+
         // to hold the console window even after the program ends
         Console.ReadLine();
     }
